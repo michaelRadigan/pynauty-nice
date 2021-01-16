14aa134b64209ae020fed8b11f433090af8c2656
Michael Radigan <michael@radigan.co.uk>
2021-01-16 18:15:09 +0000

    Initial commit with working sdist setup

---
+++ setup.py
@@ -0,0 +1,116 @@
+import os
+#from distutils.core import setup, Extension
+#from distutils.command.install import install as DistutilsInstall
+from setuptools import setup, Extension
+from setuptools.command.install import install as DistutilsInstall
+import glob
+from subprocess import check_call, check_output
+
+MODULE = 'pynauty'
+VERSION = '0.1.0'
+
+description = 'Automorphism and isomorphism of graphs'
+long_description = '''
+Package for testing isomorphism of graphs
+and computing their automorphism group.
+'''
+author = 'Peter Dobsan'
+author_email = 'pdobsan@gmail.com'
+url = 'https://github.com/pdobsan/pynauty'
+license = 'GNU General Public License v3'
+platforms = ['Linux', 'Unix', 'OS X']
+classifiers = [
+    'Environment :: Console',
+    'Operating System :: POSIX :: Linux',
+    'Operating System :: Unix',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: C',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Topic :: Scientific/Engineering :: Computing Science',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Education',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+]
+
+pynauty_dir = 'src'
+package_dir = {MODULE: pynauty_dir}
+packages = [MODULE]
+
+install_requires = []
+real_path = os.path.dirname(os.path.realpath(__file__))
+
+scripts=['install.sh', 'Makefile']
+nautypath = os.path.join(real_path, 'nauty')
+
+data_files= [
+    (nautypath, [file for file in glob.iglob('nauty/*')]),
+]
+
+
+def pre():
+    check_call('./nauty/configure')
+    check_call(['make', '-C', nautypath])
+
+class MyInstall(DistutilsInstall):
+    def run(self):
+        pre()
+        #print(check_output('./nauty/configure'))
+        #print(check_output(['make', '-C', nautypath]))
+        #check_call('./' + nautypath + '/make')
+#        self.do_pre_install_stuff()
+        DistutilsInstall.run(self)
+
+nauty_dir = 'nauty'  # nauty's source directory
+if not os.access(nauty_dir, os.R_OK | os.X_OK):
+    print("Can't find nauty_dir: %s" % nauty_dir)
+    raise SystemExit(1)
+
+ext_pynauty = Extension(
+    name=MODULE + '.nautywrap',
+    sources=[pynauty_dir + '/' + 'nautywrap.c', ],
+    extra_compile_args=['-O4', '-fPIC'],
+    extra_objects=[nauty_dir + '/' + 'nauty.o',
+                   nauty_dir + '/' + 'nautil.o',
+                   nauty_dir + '/' + 'naugraph.o',
+                   nauty_dir + '/' + 'schreier.o',
+                   nauty_dir + '/' + 'naurng.o',
+                   ],
+    include_dirs=[nauty_dir, pynauty_dir]
+)
+ext_modules = [ext_pynauty]
+
+setup(name=MODULE,
+      version=VERSION,
+      description=description, 
+      long_description=long_description,
+      author=author, 
+      author_email=author_email, 
+      url=url,
+      platforms=platforms,
+      license=license,
+      package_dir=package_dir,
+      packages=packages,
+      scripts=scripts,
+      data_files=data_files,
+      ext_modules=ext_modules,
+      classifiers=classifiers,
+      cmdclass={
+        'install': MyInstall,
+      },
+      )
+
+#setup(
+#    name='pynauty',
+#    version='0.1.0',
+#    description='pynauty fork',
+    #install_requires=install_requires,
+#    packages=['src'],
+#    data_files=data_files,
+#    cmdclass={
+#        'install': MyInstall,
+#    },
+#    scripts=scripts
+#)
